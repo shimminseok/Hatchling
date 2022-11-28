52fb128401e3dc68abf2e56fc6901ac80da2f165
Shim minseok <dssda@naver.com>
2022-11-29 02:29:09 +0900

    Create CloseBox.cs

---
+++ Unity_Scripts/5_UIs/CloseBox.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseBox : MonoBehaviour
+{
+    public void CloseMapNameBox()
+    {
+        gameObject.SetActive(false);
+    }
+}
