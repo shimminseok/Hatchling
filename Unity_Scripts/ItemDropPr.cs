f96edf8c36e1c86a4c84522022d19fd89eec700b
Shim minseok <dssda@naver.com>
2022-11-29 02:21:03 +0900

    Create ItemDropPr.cs

---
+++ Unity_Scripts/ItemDropPr.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class ItemDropPr : MonoBehaviour, IDropHandler 
+{
+    [Header("외부 참조")]
+    [SerializeField] Image _slotBg;
+    [SerializeField] Image _slotIcon;
+    [Header("Parameta")]
+    [SerializeField] Color _enterColor = Color.yellow;
+
+    Color _originColor;
+    Sprite _originImage;
+    public Sprite _slotImage
+    {
+        get { return _originImage; }
+    }
+    void Start()
+    {
+        _originColor = _slotBg.color;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        _originImage = _slotIcon.sprite;
+        transform.GetChild(0).gameObject.SetActive(true);
+        Image dropImage = eventData.pointerDrag.transform.GetChild(0).GetComponent<Image>();
+        _slotIcon.sprite = dropImage.sprite;
+        _slotIcon.color = Color.white;
+        _slotBg.color = _originColor;
+    }
+}
