0c34d46db47fcb7a71c837ec3dcdd15257f21f73
Shim minseok <dssda@naver.com>
2022-11-29 02:39:46 +0900

    Create ConvertPacketFunc.cs

---
+++ Hachling_Server/ConvertPacketFunc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+
+namespace DefineServerUtility
+{
+    class ConvertPacketFunc
+    {
+        public const short _maxByte = 1024;
+        public const short _maxPerson = 100;
+
+        public static Packet CreatePack(int protocolID, long uuid, int size, byte[] data)
+        {
+            Packet pack;
+            pack._protocolID = protocolID;
+            pack._targetID = uuid;
+            pack._totalSize = (short)size;
+            pack._datas = new byte[1002];
+            if (data != null)
+            {
+                Array.Copy(data, pack._datas, data.Length);
+            }
+            return pack;
+        }
+
+        public static byte[] StructureToByteArray(object obj)
+        {
+            int datasize = Marshal.SizeOf(obj);
+            IntPtr buff = Marshal.AllocHGlobal(datasize);
+            Marshal.StructureToPtr(obj, buff, false);
+            byte[] data = new byte[datasize];
+            Marshal.Copy(buff, data, 0, datasize);
+            Marshal.FreeHGlobal(buff);
+
+            return data;
+        }
+        public static object ByteArrayToStructure(byte[] data, Type type, int size)
+        {
+            IntPtr buff = Marshal.AllocHGlobal(data.Length);
+            Marshal.Copy(data, 0, buff, data.Length);
+            object obj = Marshal.PtrToStructure(buff, type);
+            Marshal.FreeHGlobal(buff);
+            if (Marshal.SizeOf(obj) != size)
+            {
+                return null;
+            }
+            return obj;
+        }
+    }
+}
