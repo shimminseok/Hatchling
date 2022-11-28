a5aec45d488725a896838e87917775973c168214
Shim minseok <dssda@naver.com>
2022-11-29 02:39:10 +0900

    Create Program.cs

---
+++ Hachling_Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hachling_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBController db = new DBController("");
+            ServerManager server = new ServerManager(80, db);
+
+            while (server.MainProcess())
+            {
+                server.CloseSocketProcess();
+            }
+            server.ReleaseServer();
+        }
+
+    }
+}
