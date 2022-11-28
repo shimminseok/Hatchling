5c0ac1c7a031cfc31534b0fe23f2af44de00a0a0
Shim minseok <dssda@naver.com>
2022-11-29 02:33:40 +0900

    Create ObjectManager.cs

---
+++ Unity_Scripts/1_Manager/ObjectManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ObjectManager : MonoBehaviour
+{
+    static ObjectManager _uniqueInstance;
+
+    public static ObjectManager _instance
+    {
+        get { return _uniqueInstance; }
+    }
+    public List<GameObject> _monsterList;
+
+
+
+    void Awake()
+    {
+        if (_uniqueInstance == null)
+        {
+            _uniqueInstance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        _monsterList = new List<GameObject>();
+    }
+    public void Add(GameObject obj)
+    {
+        _monsterList.Add(obj);
+    }
+    public void Remove(GameObject obj)
+    {
+        _monsterList.Remove(obj);
+    }
+    public int ObjDistance(CharacterCtrl character)
+    {
+        float dis = 0;
+        if (_monsterList != null)
+        {
+            dis = Vector3.Distance(character.transform.position, character._target.transform.position);
+        }
+        return (int)dis;
+    }
+}
