ff2e1fcef611f2199bb3369792b22b24738dc58b
Shim minseok <dssda@naver.com>
2022-11-29 02:39:31 +0900

    Create DefineProtocol.cs

---
+++ Hachling_Server/DefineProtocol.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefineServerUtility
+{
+    public enum eSendMessage
+    {
+        SendUUID,
+        DuplicateID,
+        SignUpResult,
+        UserSignUpInfo,
+        UserLoginInfo,
+        DuplicateNick,
+        InsertNickName,
+        UserInfo,
+        UpdateUserInfo,
+        UpdateItemInfo,
+        UpdateMountItem,
+    }
+    public enum eReceiveMessage
+    {
+        GetUUID,
+        DuplicateID,
+        SignUpResult,
+        UserSignUpInfo,
+        UserLoginInfo,
+        DuplicateNick,
+        InsertNickName,
+        UserInfo,
+        UpdateUserInfo,
+        UpdateItemInfo,
+        UpdateMountItem,
+
+    }
+}
